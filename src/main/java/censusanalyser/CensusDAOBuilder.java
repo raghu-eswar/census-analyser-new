97cdf95948e952ec4ece938b6d4ecd6ab9f6ea52
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-18 09:48:34 +0530

    Refactored CensusDAOBuilder.java using StreamSupport and apache.commons.beanutils.

--- src/main/java/censusanalyser/CensusDAOBuilder.java
+++ src/main/java/censusanalyser/CensusDAOBuilder.java
@@ -3,16 +3,21 @@ package censusanalyser;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.stream.StreamSupport;
+
+import static org.apache.commons.beanutils.PropertyUtils.getProperty;
+import static org.apache.commons.beanutils.PropertyUtils.setProperty;
 
 public class CensusDAOBuilder {
     Map<String, CensusDAO> censusDataMap = new HashMap<>();
 
-    private enum FieldClassesList{
+    private enum FieldClassesList {
         stateCode(IndiaStateCodeCSV.class);
         private final Class _class;
         FieldClassesList(Class _class) {
@@ -28,19 +33,19 @@ public class CensusDAOBuilder {
         }
     }
 
-    public Map<String, CensusDAO> loadCensusData(String country, String... censusCsvFilePath) throws CensusAnalyserException, IllegalAccessException, NoSuchFieldException {
+    public Map<String, CensusDAO> loadCensusData(String country, String... censusCsvFilePath) throws CensusAnalyserException, IllegalAccessException {
         Class<? extends CensusCSV> countryClass = CountryCsvClasses.valueOf(country)._class;
         return loadCensusData(countryClass,censusCsvFilePath);
     }
 
-    private   <T extends CensusCSV> Map<String, CensusDAO> loadCensusData(Class<T> csvClass, String... censusCsvFilePath) throws CensusAnalyserException, IllegalAccessException, NoSuchFieldException {
+    private <T extends CensusCSV> Map<String, CensusDAO> loadCensusData(Class<T> csvClass, String... censusCsvFilePath) throws CensusAnalyserException, IllegalAccessException {
         try (Reader reader = Files.newBufferedReader(Paths.get(censusCsvFilePath[0]))) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.getCsvBuilder();
             Iterator<T> csvIterator = csvBuilder.getCsvIterator(reader, csvClass);
-            while (csvIterator.hasNext()) {
-                CensusDAO censusDAO = new CensusDAO(csvIterator.next());
-                censusDataMap.put(censusDAO.getStateName(), censusDAO);
-            }
+            Iterable<T> csvIterable = () -> csvIterator;
+            StreamSupport.stream(csvIterable.spliterator(), false)
+                         .parallel()
+                         .forEach(t -> censusDataMap.put(t.getStateName(), new CensusDAO(t)));
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
@@ -51,7 +56,7 @@ public class CensusDAOBuilder {
         return censusDataMap;
     }
 
-    private void loadEmptyFields(String[] censusCsvFilePath) throws CensusAnalyserException, IllegalAccessException, NoSuchFieldException {
+    private void loadEmptyFields(String[] censusCsvFilePath) throws CensusAnalyserException, IllegalAccessException {
         String emptyField = getEmptyFields();
         String newEmptyField = emptyField;
         if (emptyField != null) {
@@ -67,9 +72,8 @@ public class CensusDAOBuilder {
                         throw e;
                 }
             }
-            if (emptyField.equals(newEmptyField) ) {
+            if (emptyField.equals(newEmptyField) )
                 throw new CensusAnalyserException("given files having empty fields", CensusAnalyserException.ExceptionType.EMPTY_FIELDS);
-            }
             loadEmptyFields(censusCsvFilePath);
         }
     }
@@ -87,29 +91,28 @@ public class CensusDAOBuilder {
         return null;
     }
 
-    private void loadEmptyFieldData(String fieldName, String path) throws CensusAnalyserException, NoSuchFieldException, IllegalAccessException {
+    private void loadEmptyFieldData(String fieldName, String path) throws CensusAnalyserException {
         Class fieldClass = FieldClassesList.valueOf(fieldName)._class;
         loadEmptyFieldData(path, fieldClass,fieldName);
     }
 
-    private  <T> void loadEmptyFieldData(String csvFilePath, Class<T> stateCodeClass, String field) throws CensusAnalyserException, NoSuchFieldException, IllegalAccessException {
+    private  <T> void loadEmptyFieldData(String csvFilePath, Class<T> _class, String field) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.getCsvBuilder();
-            Iterator<T> csvIterator = csvBuilder.getCsvIterator(reader, stateCodeClass);
-            while (csvIterator.hasNext()) {
-                T fieldCSV = csvIterator.next();
-                Field csvField = fieldCSV.getClass().getDeclaredField(field);
-                csvField.setAccessible(true);
-                String emptyFieldData = (String) csvField.get(fieldCSV);
-                Field stateName = fieldCSV.getClass().getDeclaredField("stateName");
-                stateName.setAccessible(true);
-                CensusDAO censusDAO = censusDataMap.get(stateName.get(fieldCSV));
-                if (censusDAO != null) {
-                    Field emptyField = censusDAO.getClass().getDeclaredField(field);
-                    emptyField.setAccessible(true);
-                    emptyField.set(censusDAO,emptyFieldData);
+            Iterator<T> csvIterator = csvBuilder.getCsvIterator(reader, _class);
+            Iterable<T> csvIterable = () -> csvIterator;
+            StreamSupport.stream(csvIterable.spliterator(), false).parallel().forEach((t) -> {
+                try {
+                    String stateName = (String) getProperty(t, "stateName");
+                    CensusDAO censusDAO = censusDataMap.get(stateName);
+                    if (censusDAO != null)  {
+                        Object value = getProperty(t, field);
+                        setProperty(censusDAO, field, value);
+                    }
+                } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                    e.printStackTrace();
                 }
-            }
+            });
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
